db6797b80a162ac4113232d1117f45342d73b61b
Jovan Jevtic <jovanjevtic132@gmail.com>
2022-09-26 19:40:44 +0200

    User register

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,16 +1,64 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
+
+//* Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    })
+  }
+
+//? Desc: Registering an user
+//* Route: POST api/users
+//! Access: Public
 
 const registerUser = asyncHandler( async (req, res) => {
 
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //* Check if user is already existing
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error('User with this email already exists');
+    }
+
+    //* Password hash
+    const salt = await bcrypt.genSalt(10);
+    const hashedPwd = await bcrypt.hash(password, salt);
+
+    //* Create a user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPwd
+    });
+
+    if (user) {
+        res.status(201).json({ 
+            _id: user._id,
+            username: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 const loginUser = asyncHandler( async (req, res) => {
-
+ 
 });
 
 const getMe = asyncHandler( async (req, res) => {
-    // res.status(200).json("Hello svijete!");
     res.send("Hello svihjete!");
 });
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 
 const { getMe, getUser, loginUser, registerUser, updateUser } = require('../controllers/userControllers');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
-router.put('/:id', updateUser);
+router.get('/me', protect, getMe);
+router.put('/:id', protect, updateUser);
 router.get('/:id', getUser);
 
 module.exports = router;
