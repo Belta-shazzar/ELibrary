c75027a12bc0cf74b9cc48c5bcd44ab9dfd79bf6
Jovan Jevtic <jovanjevtic132@gmail.com>
2022-09-26 20:15:48 +0200

    User login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -55,15 +55,36 @@ const registerUser = asyncHandler( async (req, res) => {
 });
 
 const loginUser = asyncHandler( async (req, res) => {
- 
+    const { email, password } = req.body;
+
+    //* Check for user email
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+  }
 });
 
 const getMe = asyncHandler( async (req, res) => {
-    res.send("Hello svihjete!");
+    res.status(200).json(req.user);
 });
 
 const getUser = asyncHandler( async (req, res) => {
+    const user = await User.findById(req.params.id);
 
+    if (!user) {
+        res.status(404).json({ message: 'User dooes not exist' })
+    }
+    
+    res.status(200).json({ name: user.name, email: user.email })
 });
 
 const updateUser = asyncHandler( async (req, res) => {
